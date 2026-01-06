3565742e80d3716b623c74aedb90b313b3635515
Srijit Pradhan <srijitpradhan27@gmail.com>
2026-01-06 12:32:37 +0530

    first commit

---
+++ Basics/Hellobro.java
@@ -0,0 +1,13 @@
+package Basics;
+
+//Shortcut- psvm + Tab or main + Tab
+
+public class Hellobro {
+    public static void main(String[] args) {
+        System.out.println("fdbfk");
+        System.out.println("fdbfk");
+        System.out.println("fdbfk and\n fifdc");
+
+    }
+}
+// Shortcut- sout + Tab
